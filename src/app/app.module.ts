b0d986dff2b1687b1fddc0ecc3d85b3781ec9e08
GANESHNEERATI <ganeshneerati7@gmail.com>
2020-03-16 21:47:47 +0530

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from   './app.component';
 
-import { NameEditorComponent } from './name-editor/name-editor.component';
+
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,ReactiveFormsModule ],
-  declarations: [ AppComponent, NameEditorComponent, ProfileEditorComponent ],
+  declarations: [ AppComponent, ProfileEditorComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
